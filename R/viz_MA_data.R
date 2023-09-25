d73a312a324166db62b6c4e8e56c9b5c6ea18ad2
Avery Charles <acharles20@apu.edu>
2023-09-25 13:22:49 -0700

    Created the R file for the internal data set

---
+++ R/viz_MA_data.R
@@ -0,0 +1,30 @@
+#' Starts with a title for the data set
+#' 
+#' Some lines for a short concise description of the data
+#' 
+#' @docType data "Defines the type of document"
+#' 
+#' @usage data(viz_MA) "Describes here how the data set should be loaded"
+#' 
+#' @format "Gives information on the object's format"
+#' 
+#' \describe{ "Allows you to give the user a specific description of your 
+#' variables included in the data set"
+#' }
+#' 
+#' @references "Essential if you do not use artificially generated data to 
+#' indicate the source"
+#' 
+#' @keywords "Allows you to indicate keywords"
+#' 
+#' @examples "Finally gives you some room to showcase your data"
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
+#' 
