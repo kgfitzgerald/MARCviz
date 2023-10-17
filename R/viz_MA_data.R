b267097b63cd514b7e04802a4bd6151547116e6d
kgfitzgerald <kgfitzgerald@u.northwestern.edu>
2023-10-17 08:56:32 -0700

    testing with data documentation

--- R/viz_MA_data.R
+++ R/viz_MA_data.R
@@ -1,5 +1,6 @@
 #' Meta-analytic data to visualize
 #'
+#' @description
 #' Includes data for 4 meta-analyses of size k = 10, 20, 50, 100.
 #' Should filter/subset by the first column (k) to extract data for one meta-analysis
 #'
@@ -7,20 +8,12 @@
 #'
 #' @usage data(viz_MA_data)
 #'
-#' @format "data.frame with 180 rows and 15 columns"
+#' @format ## viz_MA_data
+#' "data.frame with 180 rows and 15 columns"
 #'
-#' \describe{ "Allows you to give the user a specific description of your
+#' \describe{"Allows you to give the user a specific description of your
 #' variables included in the data set"
 #' }
 #'
 #' @examples "Finally gives you some room to showcase your data"
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
-#'
+"viz_MA_data"
