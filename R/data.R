1d4c0305fd096d4d1b467b9c774f61118dce8a38
kgfitzgerald <kgfitzgerald@u.northwestern.edu>
2023-10-17 14:04:11 -0700

    update data documentation

--- R/data.R
+++ R/data.R
@@ -9,43 +9,39 @@
 #'
 #' @format An object of class \code{"data.frame"} with 180 rows and 5 columns
 #' \describe{
-#'  \item{j}{Study # j = 1, ..., k_}
-#'  \item{delta}{True treatment effect}
-#'  \item{N_j}{total # of students in study j}
-#'  \item{M_j}{# of clusters in study j}
-#'  \item{n_j}{# of students per school in study j}
-#'  \item{N_t_j}{Total # of treatment students in study j}
-#'  \item{N_c_j}{Total # of control students in study j}
-#'  \item{rho}{ICC}
-#'  \item{SE}{Standard error used to generate initial d_j estimates}
+#'  \item{k}{Number of studies in the meta-analysis. Here there are 4 meta-analytic datasets here, with k = 10, 20, 50, 100}
 #'  \item{d_j}{Treatment effect estimate in study j}
-#'  \item{se_j}{Standard error for study j (before rescaling)}
-#'  \item{w_j}{Meta-analytic weight for study j (before rescaling)}
+#'  \item{se_j}{Standard error for study j}
+#'  \item{w_j}{Meta-analytic weight for study j}
 #'  \item{w_j_perc}{Percent weight allocated to study j}
 #' }
 #' @references This data set was generated for the statistical cognition experiment in Fitzgerald, Khella, Charles, & Tipton (2024).
 #' @keywords datasets
 #' @examples
-#' k = 10
-#' viz_adj_MARC(d_j = k10_data$d_j, se_j = k10_data$se_j, 
+#' # for k = 10
+#' k10_data <- viz_MA_data %>% filter(k == 10)
+#' viz_adj_MARC(d_j = k10_data$d_j, se_j = k10_data$se_j,
 #' study_labels = c(1:10), seed = 102,
 #' x_breaks = seq(-1, 1, 0.2), x_limit = c(-1, 1))
 #'
-#' k = 20
-#' viz_adj_MARC(d_j = k20_data$d_j, se_j = k20_data$se_j, 
+#' # for k = 20
+#' k20_data <- viz_MA_data %>% filter(k == 20)
+#' viz_adj_MARC(d_j = k20_data$d_j, se_j = k20_data$se_j,
 #' study_labels = c(1:20), seed = 102,
 #' x_breaks = seq(-1, 1, 0.2), x_limit = c(-1, 1))
-#' 
-#' k = 50
-#' viz_adj_MARC(d_j = k50_data$d_j, se_j = k50_data$se_j, 
+#'
+#' # for k = 50
+#' k50_data <- viz_MA_data %>% filter(k == 50)
+#' viz_adj_MARC(d_j = k50_data$d_j, se_j = k50_data$se_j,
 #' study_labels = c(1:50), seed = 102,
 #' x_breaks = seq(-1, 1, 0.2), x_limit = c(-1, 1))
-#' 
-#' k = 100
-#' viz_adj_MARC(d_j = k100_data$d_j, se_j = k100_data$se_j, 
+#'
+#' # for k = 100
+#' k100_data <- viz_MA_data %>% filter(k == 100)
+#' viz_adj_MARC(d_j = k100_data$d_j, se_j = k100_data$se_j,
 #' study_labels = c(1:100), seed = 102,
 #' x_breaks = seq(-1, 1, 0.2), x_limit = c(-1, 1))
-#' 
+#'
 #' data(viz_MA_data)
 #' head(viz_MA_data)
 #'
